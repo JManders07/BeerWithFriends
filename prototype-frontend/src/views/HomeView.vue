3a85c645f6cdb0a633bdebdd80026a0cc5586e26
Jelle07 <113422379+JManders07@users.noreply.github.com>
2022-12-15 15:59:57 +0100

    create frontend

---
+++ prototype-frontend/src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <b-card v-for="beer in beerList" :key="beer.amount"
+                            :title="beer.name"
+                            tag="beer"
+                            class="mb-2 allCards"
+                            align="center">
+                        <b-card-text>
+                            {{beer.description}}
+                        </b-card-text>
+                    </b-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+    import VueAxios from 'vue-axios'
+    import Vue from 'vue'
+
+    Vue.use(VueAxios, axios)
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  },
+  data() {
+    return {
+      beerList: null,
+    }
+  },
+  mounted(){
+    axios
+      .get('https://localhost:7168/api/beers/allbeers')
+      .then(response=> {
+        this.beerList = response.data
+        console.warn(this.beerList)
+      })
+  }
+}
+</script>
