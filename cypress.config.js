2d9053ca5139e2ca104819513556d8ac5ae7669b
Jelle07 <113422379+JManders07@users.noreply.github.com>
2023-01-10 11:42:11 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "gn3xng",
   e2e: {
       e2e: {
         baseUrl: 'http://localhost:8080'
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,10 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 // eslint-disable-next-line no-undef, no-unused-vars
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from failing the test
-    return false
-  })
+
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
